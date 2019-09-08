d5cbb5f64de2ff1bc5151293660cfeba7903c88b
nadaalotaibii <naaadaa_tb@yahoo.com>
2019-09-08 09:46:53 +0300

    retrieving data from database

--- src/component/products.js
+++ src/component/products.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
 import './Products.scss';
+import { Route, Link } from 'react-router-dom'
+import axios from 'axios';
 
-
+const Product = props =>(
+  <ul> 
+    <li> {props.product.image}</li>
+    <li> {props.product.name}</li>
+    <li> {props.product.price}</li>
+    <li> 
+    <Link to={'/edit'+props.product._id}> Edit </Link>
+    </li>
+  </ul>
+)
 
 class Products extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = { products:[] };
+  }
+   // retrive the data from database 
+  componentDidMount(){
+    axios.get('http://localhost:4000/products/')
+    .then(response=>{
+      this.setState({products: response.data})
+    }) 
+    .catch(function(error){
+      console.log(error);
+    })
+
+  }
+
+  productList(){
+    return this.state.products.map(function(currentProduct,i){
+      return <Product product ={currentProduct} key={i} />;
+    });
+  }
     render(){
   return (
+
+
     <div className="Product-Wrapper">
     <div className="Product">
         <div className="Product-Image-Wrapper">
@@ -20,6 +55,7 @@ class Products extends Component {
         <button onClick="" className="product-button Product-Add">Add to Cart</button>
       </div>
     </div>
+ {this.productList()}
     </div>
 
   );
@@ -28,3 +64,5 @@ class Products extends Component {
 
 
 export default Products;
+
+
